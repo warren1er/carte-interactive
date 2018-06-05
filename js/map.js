7a234bc3be15d68a2ea71a402a0c8b3ea9914b29
warren1er <kbousardo@gmail.com>
2018-06-05 18:58:56 +0200

    petite modif sur le code de la carte

--- js/map.js
+++ js/map.js
@@ -1,6 +1,5 @@
-
         // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map'), {
+         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
