8282e42164373abe3b59fd21d17886ed230a16d9
warren1er <kbousardo@gmail.com>
2018-06-12 16:44:49 +0200

    Modification afin de trouver le moyen de regler le probleme precedent

--- js/map.js
+++ js/map.js
@@ -1,7 +1,15 @@
 var map;
 function initMap() {
+  var lyon = {lat: 45.764043, lng: 4.835659};
+
   map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
+  center: lyon,
+  zoom: 12
   });
+
+  var marker = new google.maps.Marker({
+    position: lyon,
+    map:map
+  });
+  console.log(lyon);
 }
